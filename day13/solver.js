8870bc8031415e021a7417bb6cb9c40158458db2
alisonchou <alchousr21@herricksk12.org>
2020-12-22 22:32:05 -0500

    day 13

--- day13/solver.js
+++ day13/solver.js
@@ -1,8 +1,8 @@
 function solve(input, part) {
     const startTime = Number(input[0])
-    input = input[1].split(',').map(s => Number(s)).filter(s => !isNaN(s))
-    console.log(input)
+    input = input[1].split(',')
     if (part === 1) {
+        input = input.map(s => Number(s)).filter(s => !isNaN(s))
         let time = startTime
         while (true) {
             const bus = input.find(b => time % b === 0)
@@ -12,9 +12,27 @@ function solve(input, part) {
             time++
         }
     } else {
+        input = input.map((s, index) => s === 'x' ? '' : [Number(s), index])
+            .filter(s => s.length > 0)
+        let multiple = input[0][0]
+        let base = input[0][0]
+        for (let index = 1; index < input.length; index++) {
+            let i = 0
+            while (true) {
+                const increment = input[index][1]
+                const testAns = (base + multiple * i + increment)
+                if (testAns % input[index][0] === 0) {
+                    multiple *= input[index][0]
+                    base = testAns - increment
+                    break
+                }
+                i++
+            }
+        }
+        return base
     }
 }
 
-const expected = part => part === 1 ? 5946 : -1
+const expected = part => part === 1 ? 5946 : 645338524823718
 
 module.exports = { solve, expected }
